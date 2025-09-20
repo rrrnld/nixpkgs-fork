72b645a770577f63f0e88fbbee9bf266b896f169
arne <noreply@null.arnes.space>
2025-09-21 00:39:41 +0200

    codexctl: fix formatting

--- pkgs/applications/misc/remarkable/codexctl/default.nix
+++ pkgs/applications/misc/remarkable/codexctl/default.nix
@@ -1,10 +1,18 @@
-{ lib, buildPythonApplication, buildPythonPackage, callPackage, fetchPypi, fetchFromGitHub, python, rustPlatform, protobuf_27 }:
-
-let
-  protobuf = (protobuf_27.override {
+{
+  lib,
+  buildPythonApplication,
+  buildPythonPackage,
+  callPackage,
+  fetchPypi,
+  fetchFromGitHub,
+  python,
+  rustPlatform,
+  protobuf_27,
+}: let
+  protobuf = protobuf_27.override {
     version = "27.3";
     hash = "sha256-JZtRNGjAns1VJ+0831AzgOaBQkHAnnLxeXs4SoXlXpE=";
-  });
+  };
   python-ext4 = buildPythonPackage rec {
     pname = "python-ext4";
     version = "1.0.6";
@@ -39,22 +47,23 @@ let
       inherit pname version;
       hash = "sha256-L5ByWJU7pgqVqC1WM3JrR8gfLVz42IAbCSV5AW11f0o=";
     };
-    nativeBuildInputs = [ python.pkgs.setuptools-scm ];
+    nativeBuildInputs = [python.pkgs.setuptools-scm];
   };
   cryptography = (
-    (python.pkgs.cryptography.override ({
+    (python.pkgs.cryptography.override {
       cryptography-vectors =
         (callPackage ../../../../development/python-modules/cryptography/vectors.nix {})
-          .overridePythonAttrs (rec {
-            version = "44.0.1";
-            src = fetchPypi {
-              pname = "cryptography_vectors";
-              inherit version;
-              hash = "sha256-WphmsURl3PrxK837wzkph7tVnzesi4pMm2NZvno9fqA=";
-            };
-          });
-        })
-    ).overridePythonAttrs (prev: rec {
+        .overridePythonAttrs
+        rec {
+          version = "44.0.1";
+          src = fetchPypi {
+            pname = "cryptography_vectors";
+            inherit version;
+            hash = "sha256-WphmsURl3PrxK837wzkph7tVnzesi4pMm2NZvno9fqA=";
+          };
+        };
+    }).overridePythonAttrs
+    (prev: rec {
       version = "44.0.1";
       src = fetchPypi {
         inherit (prev) pname;
@@ -67,8 +76,9 @@ let
         inherit version src;
         hash = "sha256-hjfSjmwd/mylVZKyXsj/pP2KvAGDpfthuT+w219HAiA=";
       };
-    }));
-    remarkable-update-image = buildPythonPackage rec {
+    })
+  );
+  remarkable-update-image = buildPythonPackage rec {
     pname = "remarkable-update-image";
     version = "1.1.5";
     format = "pyproject";
@@ -79,7 +89,7 @@ let
       hash = "sha256-nnKJYFMvcArmQFxqx+DsTY9iMJp6rffNtCOST+I6L+g=";
     };
 
-    patches = [ ./remarkable_update_image.patch ];
+    patches = [./remarkable_update_image.patch];
 
     buildPhase = ''
       runHook pypaBuildPhase
@@ -144,57 +154,61 @@ let
       remarkable-update-image
     ];
   };
-in buildPythonApplication rec {
-  pname = "codexctl";
-  version = "1752948641";
-  format = "pyproject";
+in
+  buildPythonApplication rec {
+    pname = "codexctl";
+    version = "1752948641";
+    format = "pyproject";
 
-  src = fetchFromGitHub {
-    owner = "jayy001";
-    repo = pname;
-    rev = version;
-    hash = "sha256-K8HF5y4f0ybKc3fquRk4uWGAjj66yFC6/E4RUXvjF0w=";
-  };
+    src = fetchFromGitHub {
+      owner = "jayy001";
+      repo = pname;
+      rev = version;
+      hash = "sha256-K8HF5y4f0ybKc3fquRk4uWGAjj66yFC6/E4RUXvjF0w=";
+    };
 
-  nativeBuildInputs = [
-    python.pkgs.poetry-core
-  ];
+    nativeBuildInputs = [
+      python.pkgs.poetry-core
+    ];
 
-  propagatedBuildInputs = with python.pkgs; [
-    ((paramiko.override { inherit cryptography; }).overridePythonAttrs (prev: rec {
-      version = "3.4.1";
-      src = fetchPypi {
-        inherit (prev) pname;
-        inherit version;
-        hash = "sha256-ixUwKHCvf2ZS8uA4l1wdKXPwYEbLXX1lNVZos+y+zgw=";
-      };
-      passthru.optional-dependencies.ed25519 = [ python.pkgs.pynacl python.pkgs.bcrypt ];
-    }))
-    (psutil.overridePythonAttrs (prev: rec {
-      version = "6.0.0";
-      src = fetchPypi {
-        inherit (prev) pname;
-        inherit version;
-        hash = "sha256-j6rk8xC22Wn6JsoFRTOLIfc8axXbfEqNk0pUgvqoGPI=";
-      };
-    }))
-    (requests.overridePythonAttrs (prev: rec {
-      version = "2.32.4";
-      src = fetchPypi {
-        inherit (prev) pname;
-        inherit version;
-        hash = "sha256-J9AxZoLIopg00yZIIAJLYqNpQgg9Usry8UwFkTNtNCI=";
-      };
-    }))
-    loguru
-    remarkable-update-image
-    remarkable-update-fuse
-  ];
+    propagatedBuildInputs = with python.pkgs; [
+      ((paramiko.override {inherit cryptography;}).overridePythonAttrs (prev: rec {
+        version = "3.4.1";
+        src = fetchPypi {
+          inherit (prev) pname;
+          inherit version;
+          hash = "sha256-ixUwKHCvf2ZS8uA4l1wdKXPwYEbLXX1lNVZos+y+zgw=";
+        };
+        passthru.optional-dependencies.ed25519 = [
+          python.pkgs.pynacl
+          python.pkgs.bcrypt
+        ];
+      }))
+      (psutil.overridePythonAttrs (prev: rec {
+        version = "6.0.0";
+        src = fetchPypi {
+          inherit (prev) pname;
+          inherit version;
+          hash = "sha256-j6rk8xC22Wn6JsoFRTOLIfc8axXbfEqNk0pUgvqoGPI=";
+        };
+      }))
+      (requests.overridePythonAttrs (prev: rec {
+        version = "2.32.4";
+        src = fetchPypi {
+          inherit (prev) pname;
+          inherit version;
+          hash = "sha256-J9AxZoLIopg00yZIIAJLYqNpQgg9Usry8UwFkTNtNCI=";
+        };
+      }))
+      loguru
+      remarkable-update-image
+      remarkable-update-fuse
+    ];
 
-  meta = with lib; {
-    description = "A tool to modify remarkable device versions";
-    homepage = "https://github.com/jayy001/codexctl";
-    license = licenses.gpl3;
-    maintainers = [ ];
-  };
-}
+    meta = with lib; {
+      description = "A tool to modify remarkable device versions";
+      homepage = "https://github.com/jayy001/codexctl";
+      license = licenses.gpl3;
+      maintainers = [];
+    };
+  }
